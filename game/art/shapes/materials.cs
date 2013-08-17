fd08f592c7d6118b4ce8df5a44c7de1261b95aed
kottlerg <dev.9339@hotmail.com>
2013-08-17 15:35:46 +0800

    DUMMY house added/Build files fixed
    added the DUMMY model for the exterior of the house with the light for
    where the cellar entry will go
    
    fixed the build paths and build VS files

---
+++ game/art/shapes/materials.cs
@@ -0,0 +1,10 @@
+
+singleton Material(DUMMY_House_exterior_Material_material)
+{
+   mapTo = "Material-material";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
