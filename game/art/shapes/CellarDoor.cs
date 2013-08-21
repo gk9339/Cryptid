6706f9f7e558f7da8970b52c6505269884409c6e
kottlerg <dev.9339@hotmail.com>
2013-08-21 20:12:51 +0800

    Updated permissions/included cached DTS's

--- game/art/shapes/CellarDoor.cs
+++ game/art/shapes/CellarDoor.cs
@@ -1,3 +1,6 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2013 TGL
+//-----------------------------------------------------------------------------
 datablock StaticShapeData (cellarDoor)
 {
    shapeFile = "./DUMMYcellardoorNOANIM.cached.dts";
