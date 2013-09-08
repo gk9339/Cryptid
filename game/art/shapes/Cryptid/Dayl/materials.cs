b0c140247a9fb03c884f96da0e79ad8d5390ce61
kottlerg <dev.9339@hotmail.com>
2013-09-08 16:28:46 +0800

    level and script changes

--- game/art/shapes/Cryptid/Dayl/materials.cs
+++ game/art/shapes/Cryptid/Dayl/materials.cs
@@ -28,3 +28,13 @@ singleton Material(SidewaysBarrel_Metal_material)
    doubleSided = "1";
    translucentBlendOp = "None";
 };
+
+singleton Material(Window_Glass_material)
+{
+   mapTo = "Glass-material";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
