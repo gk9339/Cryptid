5df9b13c2b1e72b3c8b0e1ce395f8023a814803d
kottlerg <dev.9339@hotmail.com>
2013-09-08 16:17:28 +0800

    Level Changes/simGroup filing
    added lights and simGroup filing

---
+++ game/art/shapes/Cryptid/Dayl/materials.cs
@@ -0,0 +1,30 @@
+
+singleton Material(SidewaysBarrel_Wood_material)
+{
+   mapTo = "Wood-material";
+   diffuseColor[0] = "0.389509 0.22629 0.1355 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(SidewaysBarrel_Holder_material)
+{
+   mapTo = "Holder-material";
+   diffuseColor[0] = "0.213403 0.1296 0.0599269 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(SidewaysBarrel_Metal_material)
+{
+   mapTo = "Metal-material";
+   diffuseColor[0] = "0.236548 0.236548 0.236548 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
