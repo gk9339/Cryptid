2c49ee3c34dcfc9921a23b17f2594360d38ace76
kottlerg <dev.9339@hotmail.com>
2013-09-15 17:22:40 +0800

    Mission Changes (added smallbarrels, Car and Gate)

--- game/art/shapes/Cryptid/Cellar/materials.cs
+++ game/art/shapes/Cryptid/Cellar/materials.cs
@@ -9,3 +9,13 @@ singleton Material(CellarInteriors_WALLS_material)
    translucentBlendOp = "None";
    diffuseMap[0] = "art/shapes/Cryptid/Cellar/BrickOldRounded0134_2_S.jpg";
 };
+
+singleton Material(SmallBarrels_Wood)
+{
+   mapTo = "Wood";
+   diffuseColor[0] = "0.64 0.294441 0.0853333 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
--- game/art/shapes/Cryptid/Exterior/materials.cs
+++ game/art/shapes/Cryptid/Exterior/materials.cs
@@ -79,3 +79,238 @@ singleton Material(DUMMYcellardoor_Material_004_material)
    doubleSided = "1";
    translucentBlendOp = "None";
 };
+
+singleton Material(CarAndGate_wire_145028177)
+{
+   mapTo = "wire_145028177";
+   diffuseColor[0] = "0.380392 0.380392 0.380392 1";
+   specular[0] = "0.17451 0.17451 0.17451 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_wire_028028177)
+{
+   mapTo = "wire_028028177";
+   diffuseColor[0] = "0.364706 0.364706 0.364706 1";
+   specular[0] = "0.17451 0.17451 0.17451 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Jantes)
+{
+   mapTo = "Jantes";
+   diffuseColor[0] = "0.654902 0.654902 0.654902 1";
+   specular[0] = "0.636719 0.636719 0.636719 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Jantes_noir)
+{
+   mapTo = "Jantes_noir";
+   diffuseColor[0] = "0.254902 0.254902 0.254902 1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Pneus)
+{
+   mapTo = "Pneus";
+   diffuseColor[0] = "0.152941 0.152941 0.152941 1";
+   specular[0] = "0.175781 0.175781 0.175781 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Carrosserie)
+{
+   mapTo = "Carrosserie";
+   diffuseColor[0] = "0.278431 0.305882 0.388235 1";
+   specular[0] = "0.535156 0.535156 0.535156 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Carrosserie_Bouclier_arrire)
+{
+   mapTo = "Carrosserie_Bouclier_arrire";
+   diffuseColor[0] = "0.278431 0.305882 0.388235 1";
+   specular[0] = "0.535156 0.535156 0.535156 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Calandre_noir)
+{
+   mapTo = "Calandre_noir";
+   diffuseColor[0] = "0.156863 0.156863 0.156863 1";
+   specular[0] = "0.21875 0.21875 0.21875 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Carrosserie_Bouclier_avant)
+{
+   mapTo = "Carrosserie_Bouclier_avant";
+   diffuseColor[0] = "0.278431 0.305882 0.388235 1";
+   specular[0] = "0.535156 0.535156 0.535156 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Carrosserie_Capot)
+{
+   mapTo = "Carrosserie_Capot";
+   diffuseColor[0] = "0.278431 0.305882 0.388235 1";
+   specular[0] = "0.535156 0.535156 0.535156 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Dessous)
+{
+   mapTo = "Dessous";
+   diffuseColor[0] = "0.156863 0.156863 0.156863 1";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Joints_vitres)
+{
+   mapTo = "Joints_vitres";
+   diffuseColor[0] = "0.145098 0.145098 0.145098 1";
+   specular[0] = "0.21875 0.21875 0.21875 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Rtros)
+{
+   mapTo = "Rtros";
+   diffuseColor[0] = "0.180392 0.180392 0.180392 1";
+   specular[0] = "0.21875 0.21875 0.21875 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Vitres)
+{
+   mapTo = "Vitres";
+   diffuseColor[0] = "0.0196078 0.0196078 0 0.484375";
+   specular[0] = "0.996094 0.996094 0.996094 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucent = "1";
+};
+
+singleton Material(CarAndGate_Phares)
+{
+   mapTo = "Phares";
+   diffuseColor[0] = "0.568627 0.568627 0.568627 1";
+   specular[0] = "0.359375 0.359375 0.359375 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Clignotants)
+{
+   mapTo = "Clignotants";
+   diffuseColor[0] = "0.529412 0.333333 0.152941 1";
+   specular[0] = "0.359375 0.359375 0.359375 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Logo)
+{
+   mapTo = "Logo";
+   diffuseColor[0] = "0.545098 0.545098 0.545098 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Plaque)
+{
+   mapTo = "Plaque";
+   diffuseColor[0] = "0.172549 0.172549 0.172549 1";
+   specular[0] = "0.0585938 0.0585938 0.0585938 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Rtros_mirroir)
+{
+   mapTo = "Rtros_mirroir";
+   diffuseColor[0] = "0.486275 0.486275 0.486275 1";
+   specular[0] = "0.636719 0.636719 0.636719 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Feux_rouge)
+{
+   mapTo = "Feux_rouge";
+   diffuseColor[0] = "0.45098 0.141177 0.141177 1";
+   specular[0] = "0.386719 0.386719 0.386719 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_Essuis_glace)
+{
+   mapTo = "Essuis-glace";
+   diffuseColor[0] = "0.145098 0.145098 0.145098 1";
+   specular[0] = "0.128906 0.128906 0.128906 1";
+   specularPower[0] = "128";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_wire_153228214)
+{
+   mapTo = "wire_153228214";
+   diffuseColor[0] = "0.243137 0.243137 0.243137 1";
+   specular[0] = "0.17451 0.17451 0.17451 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_wire_087225087)
+{
+   mapTo = "wire_087225087";
+   diffuseColor[0] = "0.509804 0.513726 0.509804 1";
+   specular[0] = "0.17451 0.17451 0.17451 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(CarAndGate_wire_087225143)
+{
+   mapTo = "wire_087225143";
+   diffuseColor[0] = "0.207843 0.207843 0.207843 1";
+   specular[0] = "0.17451 0.17451 0.17451 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
