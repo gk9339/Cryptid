45c3877cdbb96430bd2e98c9a4cdd01a8d5c8f29
gk9339 <kottlerg@student.tranby.wa.edu.au>
2013-09-16 20:53:07 +0800

    Update cellardoor.cs

--- game/art/datablocks/cellardoor.cs
+++ game/art/datablocks/cellardoor.cs
@@ -2,7 +2,6 @@
 // Copyright (c) 2013 TGL
 //----------------------------------------------------------------------------
 echo ("Cellar Entrance Door");
-moveMap.bind(mouse0, "button0", openDoor);
 
 singleton TSShapeConstructor(DUMMYcellardoorNOANIM_cachedDts)
 {
