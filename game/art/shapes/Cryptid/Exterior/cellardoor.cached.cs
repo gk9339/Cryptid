488921e7c828bc5e24e3c3b5a20ec4068fe48724
gk9339 <kottlerg@student.tranby.wa.edu.au>
2013-09-18 18:34:23 +0800

    Added comments and changed cellardoor.cached.cs to make it integrated with the door

--- game/art/shapes/Cryptid/Exterior/cellardoor.cached.cs
+++ game/art/shapes/Cryptid/Exterior/cellardoor.cached.cs
@@ -1,30 +1,39 @@
 //-----------------------------------------------------------------------------
 // Copyright (c) 2013 TGL
 //----------------------------------------------------------------------------
+import"../../../datablocks/cellardoor.cs";
 echo ("Cellar Entrance Door");
 
+//This is a method called by t3d which makes baseShape for the cellardoor
 singleton TSShapeConstructor(DUMMYcellardoorNOANIM_cachedDts)
 {
    baseShape = "./DUMMYcellardoorNOANIM.cached.dts";
 };
 
+//This is a method called by t3d which sets animation sequences(from internal
+//or external files)
 function DUMMYcellardoorNOANIM_cachedDts::onLoad(%this)
 {
    %this.addSequence("./DUMMYcellardoor.dae", "Open", "0", "-1", "1", "0");
 }
 
-datablock StaticShapeData (cellarDoor)
-{
-   shapeFile = "./DUMMYcellardoorNOANIM.cached.dts";
-   Unopened = true;
-   
-};
+//This is the datablock for the cellardoor, and is obselete as the datablock 
+//is now in art/datablocks/cellardoor.cs
+//datablock StaticShapeData (cellarDoor)
+//{
+//   shapeFile = "./DUMMYcellardoorNOANIM.cached.dts";
+//   Unopened = true;
+//   
+//};
 
-datablock TriggerData(cellarDoorTrigger)
-{
-   tickPeriodMS = 1000;
-};
+//This is the datablock for the trigger, obselete for the same reason
+//datablock TriggerData(cellarDoorTrigger)
+//{
+//   tickPeriodMS = 1000;
+//};
 
+//This function adds the staticShape object into the world and adds the trigger
+//into the world and gives them pos scale and rot
 function addCellarDoor()
 {
    %doorObj = new StaticShape()
@@ -44,6 +53,8 @@ function addCellarDoor()
       doorID = %doorObj;
    };
 }
+
+//This function makes the cellarDoor object play the "open" animation
 function openDoor(%id)
 {
    if (cellarDoor.Unopened == true)
@@ -52,14 +63,22 @@ function openDoor(%id)
       cellarDoor.Unopened = false;
    }
 }
+
+//This would make the door play a close animation but there is none
 function closeDoor(%id)
 {
    //%id.playThread(0, "ambient");
 }
+
+//This is the function called on entry to the cellardoortrigger and calls
+//opendoor
 function cellarDoorTrigger::onEnterTrigger(%trigger, %this, %obj)
 {
    openDoor(%this.doorID);
 }
+
+//This is the function called on leaving the cellardoortrigger and calls
+//closedoor
 function cellarDoorTrigger::onLeaveTrigger(%trigger, %this, %obj)
 {
    closeDoor(%this.doorID);
