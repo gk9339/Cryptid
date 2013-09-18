3b5a4d6e19869cdc0cbe42ab7f44b71b7eb0b127
gk9339 <kottlerg@student.tranby.wa.edu.au>
2013-09-18 18:36:05 +0800

    Made cellardoor.cs datablock only a datablock

--- game/art/datablocks/cellardoor.cs
+++ game/art/datablocks/cellardoor.cs
@@ -1,18 +1,6 @@
 //-----------------------------------------------------------------------------
 // Copyright (c) 2013 TGL
 //----------------------------------------------------------------------------
-echo ("Cellar Entrance Door");
-
-singleton TSShapeConstructor(DUMMYcellardoorNOANIM_cachedDts)
-{
-   baseShape = "../shapes/Cryptid/DUMMYcellardoorNOANIM.cached.dts";
-};
-
-function DUMMYcellardoorNOANIM_cachedDts::onLoad(%this)
-{
-   %this.addSequence("../shapes/Cryptid/DUMMYcellardoor.dae", "Open", "0", "-1", "1", "0");
-}
-
 datablock StaticShapeData (cellarDoor)
 {
    shapeFile = "../shapes/Cryptid/DUMMYcellardoorNOANIM.cached.dts";
@@ -20,47 +8,9 @@ datablock StaticShapeData (cellarDoor)
    
 };
 
+//tickPeriodMS is the amount of time it takes to update the enter and exit 
+//methods in milliseconds
 datablock TriggerData(cellarDoorTrigger)
 {
    tickPeriodMS = 1000;
 };
-
-function addCellarDoor()
-{
-   %doorObj = new StaticShape()
-   {
-      position = "10.4507 43.9171 8.65399";
-      scale = "1 1 1";
-      rotation = "0 0 1 -90";
-      dataBlock = cellarDoor;
-   };
-   %Obj = new Trigger()
-   {
-      position = "8.85762 47.0466 7.31838";
-      scale = "2.48964 3.17406 2";
-      rotation = "1 0 0 0";
-      dataBlock = cellarDoorTrigger;
-      polyhedron = "0.0000000 0.0000000 0.0000000 2.0000000 0.0000000 0.0000000 0.0000000 -2.0000000 0.0000000 0.0000000 0.0000000 2.0000000";
-      doorID = %doorObj;
-   };
-}
-function openDoor(%id)
-{
-   if (cellarDoor.Unopened == true)
-   {
-      %id.playThread(0, "open");
-      cellarDoor.Unopened = false;
-   }
-}
-function closeDoor(%id)
-{
-   //%id.playThread(0, "ambient");
-}
-function cellarDoorTrigger::onEnterTrigger(%trigger, %this, %obj)
-{
-   openDoor(%this.doorID);
-}
-function cellarDoorTrigger::onLeaveTrigger(%trigger, %this, %obj)
-{
-   closeDoor(%this.doorID);
-}
